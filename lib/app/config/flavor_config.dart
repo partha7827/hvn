cb9490c4f1fe1a28a29df1522652c7e09ae44d9d
billey_b <bastien.billey@gmail.com>
2020-04-25 10:42:17 +0800

    Add build flavor for dev / prod environment

---
+++ lib/app/config/flavor_config.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+enum Flavor { DEV, PROD }
+
+class FlavorValues {
+  FlavorValues({@required this.baseUrl});
+
+  final String baseUrl;
+//Add other flavor specific values
+}
+
+class FlavorConfig {
+  final Flavor flavor;
+  final FlavorValues values;
+  static FlavorConfig _instance;
+
+  factory FlavorConfig(
+      {@required Flavor flavor, @required FlavorValues values}) {
+    _instance ??= FlavorConfig._internal(flavor, values);
+    return _instance;
+  }
+
+  FlavorConfig._internal(this.flavor, this.values);
+
+  static FlavorConfig get instance {
+    return _instance;
+  }
+
+  static bool isProduction() => _instance.flavor == Flavor.PROD;
+
+  static bool isDevelopment() => _instance.flavor == Flavor.DEV;
+}
