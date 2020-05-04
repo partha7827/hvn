efc025fd8e93f51eff5d9b5f134c44e3e83529d6
Igor Berlenko <igor@berlenko.com>
2020-05-04 13:24:54 +0800

    communicate with creator

--- lib/app/profile/chat/chat_page.dart
+++ lib/app/profile/chat/chat_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import 'chat_controller.dart';
 import 'package:highvibe/values/Strings.dart';
 import 'package:highvibe/values/themes.dart';
 import 'package:highvibe/widgets/custom_fab.dart';
-
 
 class ChatPage extends StatefulWidget {
   final String title;
@@ -14,14 +16,137 @@ class ChatPage extends StatefulWidget {
   _ChatPageState createState() => _ChatPageState();
 }
 
-class _ChatPageState extends ModularState<ChatPage, ChatController> {
-  //use 'controller' variable to access controller
+class _ChatPageState extends ModularState<ChatPage, ChatController> with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+  
+  @override
+  void initState() {
+    controller.init();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar: MessageComposer(),
+    // return StreamChat(
+    //     client: controller.client,
+    //     child: ChannelListPage(),
+    //   )
+
+    return Container(
+      child: Observer(
+        builder: (_) => controller.client != null
+            ? StreamChat(
+                client: controller.client,
+                child: StreamChannel(
+
+                  channel: controller.channel,
+                  child: ChannelPage(),
+                ),
+              )
+            : Center(child: CircularProgressIndicator()),
+      ),
+    );
+    // return SafeArea(
+    //   child: Scaffold(
+    //     bottomNavigationBar: MessageComposer(),
+    //   ),
+    // );
+  }
+}
+
+class ChannelListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChannelsBloc(
+        child: ChannelListView(
+          filter: {
+            'members': {
+              '\$in': [StreamChat.of(context).user.id],
+            }
+          },
+          sort: [SortOption('last_message_at')],
+          pagination: PaginationParams(
+            limit: 20,
+          ),
+          channelWidget: ChannelPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: ChannelHeader(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                MessageListView(
+                  showVideoFullScreen: false,
+                  threadBuilder: (_, parentMessage) {
+                    return ThreadPage(
+                      parent: parentMessage,
+                    );
+                  },
+                ),
+                Positioned.fill(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 4,
+                    ),
+                    child: TypingIndicator(
+                      alignment: Alignment.bottomRight,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          MessageInput(),
+          // MessageComposer()
+        ],
+      ),
+    );
+  }
+}
+
+class ThreadPage extends StatelessWidget {
+  final Message parent;
+
+  ThreadPage({
+    Key key,
+    this.parent,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ThreadHeader(
+        parent: parent,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: MessageListView(
+              parentMessage: parent,
+            ),
+          ),
+          if (parent.type != 'deleted')
+            MessageInput(
+              parentMessage: parent,
+            ),
+        ],
       ),
     );
   }
