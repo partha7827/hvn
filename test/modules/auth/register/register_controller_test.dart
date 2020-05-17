430f8c2d080a60036cf926383f0b97b012d4c1fb
Igor Berlenko <igor@berlenko.com>
2020-05-17 18:17:58 +0800

    update github workflow with testing

--- test/modules/auth/register/register_controller_test.dart
+++ test/modules/auth/register/register_controller_test.dart
@@ -1,26 +1,20 @@
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:highvibe/modules/auth/auth_module.dart';
 
 import 'package:highvibe/modules/auth/register/register_controller.dart';
-import 'package:highvibe/modules/auth/register/register_module.dart';
 
 void main() {
-  initModule(RegisterModule());
-  // RegisterController register;
-  //
+  initModule(AuthModule());
+  RegisterController register;
+
   setUp(() {
-    //     register = RegisterModule.to.get<RegisterController>();
+    register = AuthModule.to.get<RegisterController>();
   });
 
   group('RegisterController Test', () {
-    //   test("First Test", () {
-    //     expect(register, isInstanceOf<RegisterController>());
-    //   });
-
-    //   test("Set Value", () {
-    //     expect(register.value, equals(0));
-    //     register.increment();
-    //     expect(register.value, equals(1));
-    //   });
+    test("initModule", () {
+      expect(register, isInstanceOf<RegisterController>());
+    });
   });
 }
