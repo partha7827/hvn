6c96b053520085cfedc903f11ec805cbe06eafda
minikin <djminikin@gmail.com>
2020-05-26 09:37:07 +0200

    Update media_overlays

--- lib/modules/app/media_overlays.dart
+++ lib/modules/app/media_overlays.dart
@@ -1,10 +1,14 @@
 // Global properties
 import 'package:flutter/material.dart';
-import 'package:highvibe/models/models.dart' show Video;
+import 'package:highvibe/models/models.dart' show Audio, Video;
+import 'package:highvibe/modules/audio_player/audio_player_page.dart';
 import 'package:highvibe/modules/video_player/video_player_page.dart';
 
 OverlayEntry videoOverlayEntry;
 OverlayState videOverlayState;
+
+OverlayEntry audioOverlayEntry;
+OverlayState audioOverlayState;
 
 void presentVideoPlayerAsOverlay(BuildContext context, Video video) {
   videOverlayState = Overlay.of(context);
@@ -15,4 +19,15 @@ void presentVideoPlayerAsOverlay(BuildContext context, Video video) {
   );
 
   videOverlayState.insert(videoOverlayEntry);
+}
+
+void presentAudioPlayerAsOverlay(BuildContext context, Audio audioFile) {
+  audioOverlayState = Overlay.of(context);
+  audioOverlayEntry = OverlayEntry(
+    builder: (context) {
+      return AudioPlayerPage(audioFile: audioFile);
+    },
+  );
+
+  audioOverlayState.insert(audioOverlayEntry);
 }
