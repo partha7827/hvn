f397fa26e25301ea72f006061d4ea6f41133901e
Vijay Sharma <vijay@techracers.io>
2020-05-26 20:21:48 +0530

    Fix appbar title while scrolling down in profile screen

--- lib/modules/profile/other_user/other_user_page.dart
+++ lib/modules/profile/other_user/other_user_page.dart
@@ -22,6 +22,7 @@ class _OtherUserPageState
     extends ModularState<OtherUserPage, OtherUserController>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
+  var top = 0.0;
 
   @override
   void initState() {
@@ -41,129 +42,144 @@ class _OtherUserPageState
       headerSliverBuilder: (context, innerBoxIsScrolled) {
         return <Widget>[
           SliverAppBar(
-            expandedHeight: 300,
-            floating: false,
-            pinned: true,
-            backgroundColor: secondaryColor,
-            leading: BackButton(
-              onPressed: ProfileModule.toHome,
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              collapseMode: CollapseMode.parallax,
-              stretchModes: [
-                StretchMode.fadeTitle,
-              ],
-              background: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    AppBar(
-                      backgroundColor: Colors.transparent,
-                      leading: Container(),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Hero(
-                          tag: "author#${controller.otherUser.id}",
-                          child: ProfileAvatar(controller.otherUser.photoUrl),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 16.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                controller.otherUser.name,
-                                style: bold20White,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 4.0),
-                                child: Text(
-                                  controller.otherUser.status,
-                                  style: normal16Hint,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8.0),
-                                child: Container(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  child: GradientOutlineButton(
-                                    label: Observer(
-                                      builder: (_) => Text(
-                                        controller.isFollowing
-                                            ? Strings.unfollow
-                                            : Strings.follow,
-                                        style: normal16Accent,
-                                      ),
-                                    ),
-                                    onPressed: controller.followUser,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 20),
+              expandedHeight: 300,
+              floating: false,
+              pinned: true,
+              backgroundColor: secondaryColor,
+              leading: BackButton(
+                onPressed: ProfileModule.toHome,
+              ),
+              flexibleSpace: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  top = constraints.biggest.height;
+                  return FlexibleSpaceBar(
+                    collapseMode: CollapseMode.parallax,
+                    stretchModes: [
+                      StretchMode.fadeTitle,
+                    ],
+                    title: Opacity(
+                      opacity: top > 100 ? 0 : 1,
                       child: Text(
-                        controller.otherUser.bio,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: normal16Hint,
+                        controller.otherUser.name,
+                        style: bold20White,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Observer(builder: (_) {
-                          return InkWell(
-                            onTap: () {
-                              if (controller.followers.length > 0)
-                                ProfileModule.toFollowers(controller.followers);
-                            },
-                            child: Column(
-                              children: <Widget>[
-                                Text(
-                                  controller.followers.length.toString(),
-                                  style: bold20White,
+                    background: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          AppBar(
+                            backgroundColor: Colors.transparent,
+                            leading: Container(),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Hero(
+                                tag: "author#${controller.otherUser.id}",
+                                child: ProfileAvatar(
+                                    controller.otherUser.photoUrl),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      controller.otherUser.name,
+                                      style: bold20White,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 4.0),
+                                      child: Text(
+                                        controller.otherUser.status,
+                                        style: normal16Hint,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8.0),
+                                      child: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.3,
+                                        child: GradientOutlineButton(
+                                          label: Observer(
+                                            builder: (_) => Text(
+                                              controller.isFollowing
+                                                  ? Strings.unfollow
+                                                  : Strings.follow,
+                                              style: normal16Accent,
+                                            ),
+                                          ),
+                                          onPressed: controller.followUser,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                Text('Followers', style: normal16Hint)
-                              ],
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            child: Text(
+                              controller.otherUser.bio,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: normal16Hint,
                             ),
-                          );
-                        }),
-                        Observer(builder: (_) {
-                          return InkWell(
-                            onTap: () {
-                              if (controller.following.length > 0)
-                                ProfileModule.toFollowing(controller.following);
-                            },
-                            child: Column(
-                              children: <Widget>[
-                                Text(
-                                  controller.following.length.toString(),
-                                  style: bold20White,
-                                ),
-                                Text('Following', style: normal16Hint)
-                              ],
-                            ),
-                          );
-                        })
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Observer(builder: (_) {
+                                return InkWell(
+                                  onTap: () {
+                                    if (controller.followers.length > 0)
+                                      ProfileModule.toFollowers(
+                                          controller.followers);
+                                  },
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(
+                                        controller.followers.length.toString(),
+                                        style: bold20White,
+                                      ),
+                                      Text('Followers', style: normal16Hint)
+                                    ],
+                                  ),
+                                );
+                              }),
+                              Observer(builder: (_) {
+                                return InkWell(
+                                  onTap: () {
+                                    if (controller.following.length > 0)
+                                      ProfileModule.toFollowing(
+                                          controller.following);
+                                  },
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(
+                                        controller.following.length.toString(),
+                                        style: bold20White,
+                                      ),
+                                      Text('Following', style: normal16Hint)
+                                    ],
+                                  ),
+                                );
+                              })
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )),
           ProfileTabButtons(controller: _tabController),
         ];
       },
