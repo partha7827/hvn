81394f3291eb9910bc302abde16b4f77921cc869
Vijay Sharma <vijay@techracers.io>
2020-05-29 22:29:56 +0530

    Show exit confirmation dialog

---
+++ lib/modules/home/widgets/exit_app.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:highvibe/modules/wallet/resources/app_colors.dart';
+import 'package:highvibe/values/themes.dart';
+
+showExitDialog(BuildContext context) async {
+  bool shouldExit = false;
+  await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8.0),
+            ),
+          ),
+          backgroundColor: AppColors.backgroundLightBlue,
+          title: Text(
+            'Are you sure?',
+            style: bold18PlayfairWhite,
+          ),
+          actions: <Widget>[
+            DialogFlatButton(
+              title: 'Yes',
+              onPressed: () {
+                Modular.to.pop();
+                shouldExit = true;
+              },
+            ),
+            DialogFlatButton(
+              title: 'No',
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                Modular.to.pop();
+                shouldExit = false;
+              },
+            ),
+          ],
+        );
+      });
+  return shouldExit;
+}
+
+class DialogFlatButton extends StatelessWidget {
+  const DialogFlatButton({
+    Key key,
+    this.title = '',
+    @required this.onPressed,
+    this.color = Colors.transparent,
+  }) : super(key: key);
+
+  final String title;
+  final Function onPressed;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: color,
+      onPressed: onPressed,
+      child: Text(
+        '$title',
+      ),
+    );
+  }
+}
