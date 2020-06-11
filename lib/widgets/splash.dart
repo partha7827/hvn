4aa884882b5da0900a1b6e8f43e3fb2e0d2aab2d
deepikadt <deepika.tripathi@appinventiv.com>
2020-06-11 12:16:25 +0530

    splash widget added for highvibe_anmiation

---
+++ lib/widgets/splash.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key key}) : super(key: key);
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  void _navigate(){
+    Future.delayed(const Duration(milliseconds: 142),(){
+        //navigate to next screen
+   });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+       child:Center(child: Lottie.asset('assets/highvibe_animation.json',
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,),
+      ),
+     );
+  }
+
+ 
+  }
