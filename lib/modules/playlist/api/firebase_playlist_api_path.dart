fd8a0c70719c54d0e3fd084dbdc0844970524b9b
minikin <djminikin@gmail.com>
2020-06-24 16:17:57 +0200

    Add FirestoreApiService

--- lib/modules/playlist/api/firebase_playlist_api_path.dart
+++ lib/modules/playlist/api/firebase_playlist_api_path.dart
@@ -1,4 +1,4 @@
-class FirebaseApiPath {
+class FirebasePlaylistApiPath {
   static String playlist(String userId, String playlistId) =>
       'users/$userId/playlists/$playlistId';
   static String playlistItems(String userId) => 'users/$userId/playlists';
