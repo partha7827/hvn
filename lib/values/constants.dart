8af1fea4b03ab03bf2c6b948a0b2062b3d3edf81
Vijay Sharma <vijay@techracers.io>
2020-06-29 19:45:18 +0530

    Add constants for options menu

--- lib/values/constants.dart
+++ lib/values/constants.dart
@@ -1,2 +1,9 @@
 const disabledOpacity = 0.4;
 const enabledOpacity = 1.0;
+
+const edit = 0;
+const addToPlaylist = 1;
+const addWayPoints = 2;
+const addToFavorites = 3;
+const delete = 4;
+const share = 5;
