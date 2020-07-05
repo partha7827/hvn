d74c21360d3355c62a004ae1c205afdaeb35d0ca
minikin <djminikin@gmail.com>
2020-07-05 20:40:29 +0200

    Add Playlist to AudioPage

--- lib/modules/profile/audio/audio_controller.dart
+++ lib/modules/profile/audio/audio_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:highvibe/models/audio/audio.dart';
+import 'package:highvibe/modules/app/app_controller.dart';
 import 'package:highvibe/services/firestore_service.dart';
 import 'package:mobx/mobx.dart';
 
@@ -9,6 +10,7 @@ class AudioController = _AudioControllerBase with _$AudioController;
 
 abstract class _AudioControllerBase with Store {
   final FirestoreService store = Modular.get<FirestoreService>();
+  final AppController currentUserStore = Modular.get<AppController>();
 
   @observable
   ObservableFuture<List<Audio>> audios;
--- lib/modules/profile/audio/audio_page.dart
+++ lib/modules/profile/audio/audio_page.dart
@@ -5,6 +5,7 @@ import 'package:highvibe/models/models.dart';
 import 'package:highvibe/modules/app/media_overlays.dart';
 import 'package:highvibe/modules/playlist/playlist_module.dart';
 import 'package:highvibe/modules/playlist/resources/resources.dart';
+import 'package:highvibe/modules/playlist/show_playlists/show_playlists_module.dart';
 import 'package:highvibe/modules/playlist/widgets/widgets.dart';
 import 'package:highvibe/utils/utils.dart';
 import 'package:highvibe/values/strings.dart';
@@ -66,26 +67,30 @@ class _AudioPageState extends ModularState<AudioPage, AudioController> {
         ],
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const HeaderRow(title: PlaylistStrings.playlist),
-          ],
+          children: [const HeaderRow(title: PlaylistStrings.playlist)],
         ),
         const SizedBox(height: 10),
-        for (final item in tempInMemoryPlaylistCollection) ...[
-          GestureDetector(
-            onTap: () async {
-              MediaOverlays.presentPlayListPlayerAsOverlay(
-                context: context,
-                playList: item,
-              );
-            },
-            child: PlaylistTile(
-              isInEditMode: false,
-              playList: item,
-              onTap: (item) => PlaylistModule.toContextMenu(playList: item),
-            ),
-          )
-        ],
+        Container(
+          height: 500,
+          child: ShowPlaylistsModule(
+            userId: controller.currentUserStore.currentUser.id,
+          ),
+        ),
+        // for (final item in tempInMemoryPlaylistCollection) ...[
+        //   GestureDetector(
+        //     onTap: () async {
+        //       MediaOverlays.presentPlayListPlayerAsOverlay(
+        //         context: context,
+        //         playList: item,
+        //       );
+        //     },
+        //     child: PlaylistTile(
+        //       isInEditMode: false,
+        //       playList: item,
+        //       onTap: (item) => PlaylistModule.toContextMenu(playList: item),
+        //     ),
+        //   )
+        // ],
       ],
     );
   }
