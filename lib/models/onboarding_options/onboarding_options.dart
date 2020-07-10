c7a30541ecbed5e85e62452b25151501765bb6e9
Vijay Sharma <vijay@techracers.io>
2020-07-10 20:55:10 +0530

    Add model for user onboarding options

---
+++ lib/models/onboarding_options/onboarding_options.dart
@@ -0,0 +1,9 @@
+class OnboardingOptionsModel {
+  OnboardingOptionsModel({
+    this.title = '',
+    this.imagePath = '',
+    this.isSelected = false,
+  });
+  final String title, imagePath;
+  bool isSelected;
+}
