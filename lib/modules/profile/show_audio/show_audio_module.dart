e03cc1c940cf834e34247b13c6a0fe7e94559b3a
Igor Berlenko <igor@berlenko.com>
2020-07-12 00:18:18 +0800

    fix unit testing

--- lib/modules/profile/show_audio/show_audio_module.dart
+++ lib/modules/profile/show_audio/show_audio_module.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:highvibe/modules/app/app_controller.dart';
 import 'package:highvibe/modules/profile/show_audio/show_audio_controller.dart';
 import 'package:highvibe/modules/profile/show_audio/show_audio_page.dart';
+import 'package:highvibe/services/auth_service.dart';
+import 'package:highvibe/services/firestore_service.dart';
 
 class ShowAudioModule extends WidgetModule {
   final String userId;
@@ -10,6 +13,9 @@ class ShowAudioModule extends WidgetModule {
 
   @override
   List<Bind<Object>> get binds => [
+        Bind((i) => AppController()),
+        Bind((i) => AuthService.withFirebase()),
+        Bind((i) => FirestoreService.withFirebase()),
         Bind((i) => ShowAudioController(userId)),
       ];
 
