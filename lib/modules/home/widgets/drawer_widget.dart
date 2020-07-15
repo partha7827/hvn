2943d036896be88f817600d9724030503d10b8c6
Vijay Sharma <vijay@techracers.io>
2020-07-15 16:48:17 +0530

    Fix analyzer issues

--- lib/modules/home/widgets/drawer_widget.dart
+++ lib/modules/home/widgets/drawer_widget.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:highvibe/modules/home/home_controller.dart';
